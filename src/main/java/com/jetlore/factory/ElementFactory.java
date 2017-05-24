274c6ec14cb7c4deca9703293b46327538a987ff
Alex Kontsur <alexander.kontsur@gmail.com>
2017-05-24 21:57:11 +0300

    [*] concurrenty concerns removed

--- src/main/java/com/jetlore/factory/ElementFactory.java
+++ src/main/java/com/jetlore/factory/ElementFactory.java
@@ -5,7 +5,6 @@ import com.jetlore.model.Element;
 import com.jetlore.model.Item;
 
 import java.lang.reflect.Constructor;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * ElementFactory
@@ -15,25 +14,10 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class ElementFactory {
 
-    @SuppressWarnings("CollectionDeclaredAsConcreteClass")
-    private ConcurrentHashMap<Class, Constructor> cache = new ConcurrentHashMap<>();
-
     public Element buildElement(String source, ItemMeta itemMeta) throws Exception {
-        Constructor constructor = getConstructor(itemMeta.getItemType().getTargetClass());
+        Constructor constructor = itemMeta.getItemType().getTargetClass().getConstructor(Element.class);
         Item item = new Item(source.substring(itemMeta.getStart(), itemMeta.getEnd()));
         return (Element) constructor.newInstance(item);
-    }
-
-    private Constructor getConstructor(Class clazz) throws Exception {
-        Constructor constructor = cache.get(clazz);
-        if (constructor == null) {
-            constructor = clazz.getConstructor(Element.class);
-            Constructor old = cache.putIfAbsent(clazz, constructor);
-            if (old != null) {
-                constructor = old;
-            }
-        }
-        return constructor;
     }
 
 }
--- src/test/java/com/jetlore/test/FormattedTest.java
+++ src/test/java/com/jetlore/test/FormattedTest.java
@@ -9,11 +9,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 /**
  * FormattedTest
@@ -24,16 +19,14 @@ import java.util.concurrent.TimeUnit;
 public class FormattedTest {
 
     private SourceAcceptor sourceAcceptor;
-    private ExecutorService executorService;
 
     @Before
     public void init() {
         sourceAcceptor = new SourceAcceptor();
-        executorService = Executors.newFixedThreadPool(2);
     }
 
     @Test
-    public void wrongEntityBoundsProcessingFailed() throws Exception {
+    public void wrongEntityBoundsProcessingFailed() {
         String input = "Obama visited Facebook headquarters: http://bit.ly/xyz @elversatile the end";
 
         List<ItemMeta> items = new ArrayList<>();
@@ -42,8 +35,13 @@ public class FormattedTest {
         items.add(new ItemMeta(ItemType.TwitterUser, 48, 56));
         items.add(new ItemMeta(ItemType.Link, 37, 47));
 
-        String session = UUID.randomUUID().toString();
-        Assert.assertFalse(acceptInput(session, input, items));
+        Exception ex = null;
+        try {
+            sourceAcceptor.processInput(input, items);
+        } catch (Exception e) {
+            ex = e;
+        }
+        Assert.assertNotNull(ex);
     }
 
     @Test
@@ -56,11 +54,8 @@ public class FormattedTest {
         items.add(new ItemMeta(ItemType.TwitterUser, 55, 67)); //48, 56
         items.add(new ItemMeta(ItemType.Link, 37, 54)); //37, 47
 
-        String session = UUID.randomUUID().toString();
-        Assert.assertTrue(acceptInput(session, input, items));
-
         String expectation = "<strong>Obama</strong> visited <strong>Facebook</strong> headquarters: <a href=\"http://bit.ly/xyz\">http://bit.ly/xyz</a> @<a href=\"http://twitter.com/elversatile\">elversatile</a> the end";
-        Assert.assertEquals(expectation, getOutput(session));
+        Assert.assertEquals(expectation, sourceAcceptor.processInput(input, items));
     }
 
     @Test
@@ -71,8 +66,13 @@ public class FormattedTest {
         items.add(new ItemMeta(ItemType.Entity, 0, 5));
         items.add(new ItemMeta(ItemType.Entity, 5, 13));
 
-        String session = UUID.randomUUID().toString();
-        Assert.assertFalse(acceptInput(session, input, items));
+        Exception ex = null;
+        try {
+            sourceAcceptor.processInput(input, items);
+        } catch (Exception e) {
+            ex = e;
+        }
+        Assert.assertNotNull(ex);
     }
 
     @Test
@@ -85,43 +85,8 @@ public class FormattedTest {
         items.add(new ItemMeta(ItemType.TwitterUser, 55, 67));
         items.add(new ItemMeta(ItemType.Link, 37, 54));
 
-        String session = UUID.randomUUID().toString();
-
-        Assert.assertTrue(acceptInput(session, input, items));
-
         String expectation = "<strong>Obama</strong> visited <strong>Facebook</strong> headquarters: <a href=\"http://bit.ly/xyz\">http://bit.ly/xyz</a> @<a href=\"http://twitter.com/elversatile\">elversatile</a>";
-        Assert.assertEquals(expectation, getOutput(session));
-    }
-
-    @SuppressWarnings("ThrowableResultOfMethodCallIgnored")
-    private boolean acceptInput(String session, String input, List<ItemMeta> items) throws Exception{
-        Exception exception = executorService.submit(() -> {
-            Exception ex = null;
-            try {
-                sourceAcceptor.accept(session, input, items);
-            } catch (Exception e) {
-                ex = e;
-            }
-            return ex;
-        }).get();
-        return exception == null;
-    }
-
-    private String getOutput(String session) throws Exception {
-        TimeUnit.MILLISECONDS.sleep(500);
-
-        CountDownLatch latch = new CountDownLatch(1);
-        String result = executorService.submit(() -> {
-            String output = sourceAcceptor.getOutput(session);
-            latch.countDown();
-            return output;
-        }).get();
-
-        try {
-            latch.await(3, TimeUnit.SECONDS);
-        } catch (Exception ignored) {
-        }
-        return result;
+        Assert.assertEquals(expectation, sourceAcceptor.processInput(input, items));
     }
 
 }
